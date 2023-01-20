9a296c4df93fbcdbcf135df43aa3df56570106bd
Diana Dilova <dianadilovaa@gmail.com>
2023-01-20 23:31:00 +0200

    Added more tests for Sum and Unit tests for Average method.

--- Summator.UnitTests/SummatorTests.cs
+++ Summator.UnitTests/SummatorTests.cs
@@ -8,33 +8,130 @@ namespace Summator.UnitTests
         public void Test_Summator_SumTwoPositiveNumber()
         {
             var nums = new int[] { 1, 2 };
-            var actual = Summator.Sum(nums);
-
             var expected = 3;
 
-            Assert.AreEqual(expected, actual);
+            var actual = Summator.Sum(nums);
+
+            Assert.That(expected, Is.EqualTo(actual));
         }
 
         [Test]
         public void Test_Summator_SumTwoNegativeNumber()
         {
             var nums = new int[] { -1, -99 };
-            var actual = Summator.Sum(nums);
-
             var expected = -100;
 
-            Assert.AreEqual(expected, actual);
+            var actual = Summator.Sum(nums);
+
+            Assert.That(expected, Is.EqualTo(actual));
         }
 
         [Test]
-        public void Test_Summator_OneNumber()
+        public void Test_Summator_SumOneNumber()
         {
             var nums = new int[] { 6 };
-            var actual = Summator.Sum(nums);
-
             var expected = 6;
 
-            Assert.AreEqual(expected, actual);
+            var actual = Summator.Sum(nums);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void Test_Summator_SumZeroNumber()
+        {
+            var nums = new int[] { };
+            var expected = 0;
+
+            var actual = Summator.Sum(nums);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void Test_Summator_SumBigNumbers()
+        {
+            var nums = new int[] { 2000000000, 2000000000, 2000000000 };
+            var expected = 6000000000;
+
+            var actual = Summator.Sum(nums);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void Test_Summator_SumOnePositiveAndOneNegative()
+        {
+            var nums = new int[] { 5, -3 };
+            var expected = 2;
+
+            var actual = Summator.Sum(nums);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void Test_Summator_SumOnePositiveAndTwoNegative()
+        {
+            var nums = new int[] { 5, -3, -2 };
+            var expected = 0;
+
+            var actual = Summator.Sum(nums);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void Test_Summator_AveragePositiveNumbers()
+        {
+            var nums = new int[] { 5, 10, 15 };
+            var expected = 10;
+
+            var actual = Summator.Average(nums);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void Test_Summator_AverageNegativeNumbers()
+        {
+            var nums = new int[] { -5, -10, -15 };
+            var expected = -10;
+
+            var actual = Summator.Average(nums);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void Test_Summator_AverageTwoNumbers()
+        {
+            var nums = new int[] { 5, 15 };
+            var expected = 10;
+
+            var actual = Summator.Average(nums);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void Test_Summator_AverageBigNumbers()
+        {
+            var nums = new int[] { 1000000, 1500000 };
+            var expected = 1250000;
+
+            var actual = Summator.Average(nums);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+        public void Test_Summator_AverageBigNegativeNumbers()
+        {
+            var nums = new int[] { -1000000, -1500000 };
+            var expected = -1250000;
+
+            var actual = Summator.Average(nums);
+
+            Assert.That(expected, Is.EqualTo(actual));
         }
     }
 }
--- Summator/Program.cs
+++ Summator/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Summator.Test_SumTwoNumbers();
+            Console.WriteLine("The sum is equal to " + Summator.Sum(new int[] { 1, 2, 3 }));
+            Console.WriteLine("The average numbers is: " + Summator.Average(new int[] { 5, 6, 7 }));
+
         }
     }
 }
